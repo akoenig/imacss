348a38e688f8a5354516a38651b1fc70bc5a49cd
André König <andre.koenig@gmail.com>
2014-02-10 19:21:36 +0100

    Initial Revision. Created directory structure.

---
+++ index.js
@@ -0,0 +1,25 @@
+/*
+ * imacss
+ *
+ * Copyright(c) 2014 André König <andre.koenig@posteo.de>
+ * MIT Licensed
+ *
+ */
+
+/**
+ * @author André König <andre.koenig@posteo.de>
+ *
+ */
+
+'use strict';
+
+/**
+ * DOCME
+ *
+ * @param  {[type]} glob [description]
+ * @return {[type]}      [description]
+ *
+ */
+exports.convert = function convert (glob, cssfile, cssclass) {
+
+};
